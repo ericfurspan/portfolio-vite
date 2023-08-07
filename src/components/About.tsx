5601332b187814bdddb75dfde8c34a1287106689
Eric Furspan <ericfurspan@gmail.com>
2023-08-06 20:38:46 -0400

    a11y fix: h2

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import appConfig from '../config';
 const About = () => (
   <>
     {appConfig.about.map((str) => (
-      <Typography key={str} variant="subtitle1" sx={{ mb: 2 }}>
+      <Typography key={str} variant="subtitle1" component="h2" sx={{ mb: 2 }}>
         {str}
       </Typography>
     ))}
