26afe548456ac3586d2fc5680f1328d5a46167e0
Eric Furspan <eric.furspan@gmail.com>
2023-10-21 19:17:49 -0400

    increase fontSize of About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import appConfig from '../config';
 const About = () => (
   <Box>
     {appConfig.about.map((str) => (
-      <Typography variant="subtitle1" component="p" sx={{ mb: 2 }} key={str}>
+      <Typography variant="subtitle1" component="p" fontSize={'1.125rem'} sx={{ mb: 2 }} key={str}>
         {str}
       </Typography>
     ))}
